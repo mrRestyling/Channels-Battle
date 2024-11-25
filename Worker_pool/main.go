93f4c2defd6a010d34ec3fc8f13294221ff54819
Egor Onekeev <sergeev_e_k@mail.ru>
2024-11-25 17:48:10 +0300

    wg

--- Worker_pool/main.go
+++ Worker_pool/main.go
@@ -2,40 +2,48 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 
+	var wg sync.WaitGroup
+
 	tasks := make(chan int, 10)
 	results := make(chan int, 10)
 
 	for i := 1; i <= 3; i++ {
-		go sqrWorker(tasks, results, i)
+		wg.Add(1)
+		go sqr(&wg, tasks, results, i)
 	}
 
-	for i := 0; i < 5; i++ {
+	for i := 1; i <= 5; i++ {
 		tasks <- i * 2
 	}
 
-	fmt.Println("[main] Wrote 5 tasks")
+	fmt.Println("go 5 tasks")
 
 	close(tasks)
 
+	wg.Wait()
+
 	for i := 1; i <= 5; i++ {
 		result := <-results
-		fmt.Println("[main] Result", i, ":", result)
+		fmt.Println(i, " + ", result)
 	}
 
 	fmt.Println("STOP")
-
 }
 
-func sqrWorker(tasks <-chan int, results chan<- int, id int) {
+func sqr(wg *sync.WaitGroup, tasks <-chan int, results chan<- int, insta int) {
 
+	defer wg.Done()
 	for num := range tasks {
 		time.Sleep(time.Millisecond)
-		fmt.Printf("[worker %v] Sending result by worker %v\n", id, id)
-		results <- num * num * num
+		fmt.Println("--- --- ---", insta)
+		results <- num * num
+
 	}
+
 }
